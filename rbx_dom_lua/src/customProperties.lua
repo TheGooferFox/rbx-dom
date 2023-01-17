99c82092244ba66d0aaa72a1d8932c7510415fb2
Max <clonet1019@gmail.com>
2023-01-17 14:25:59 -0600

    Simplify MeshPart patching.
    Not as stable, but doing this kind of thinking retry logic isn't something rbx-dom should have to think about.

--- rbx_dom_lua/src/customProperties.lua
+++ rbx_dom_lua/src/customProperties.lua
@@ -3,7 +3,6 @@
 
 local CollectionService = game:GetService("CollectionService")
 local InsertService = game:GetService("InsertService")
-local MESH_DISPATCH = {}
 
 return {
 	Instance = {
@@ -11,6 +10,7 @@ return {
 			read = function(instance)
 				return true, instance:GetAttributes()
 			end,
+
 			write = function(instance, _, value)
 				local existing = instance:GetAttributes()
 
@@ -27,14 +27,16 @@ return {
 				return true
 			end,
 		},
+
 		Tags = {
 			read = function(instance)
 				return true, CollectionService:GetTags(instance)
 			end,
+
 			write = function(instance, _, value)
 				local existingTags = CollectionService:GetTags(instance)
-
 				local unseenTags = {}
+
 				for _, tag in ipairs(existingTags) do
 					unseenTags[tag] = true
 				end
@@ -57,6 +59,7 @@ return {
 			read = function(instance, key)
 				return true, instance:GetContents()
 			end,
+
 			write = function(instance, key, value)
 				instance:SetContents(value)
 				return true
@@ -68,72 +71,24 @@ return {
 			read = function(meshPart: MeshPart)
 				return true, meshPart.MeshId
 			end,
+
 			write = function(meshPart: MeshPart, _, meshId: string)
-				if meshPart.MeshId == meshId then
-					return
-				end
+				task.spawn(function ()
+					-- Use default settings to do this as immediately as possible.
+					local import = InsertService:CreateMeshPartAsync(meshId, Enum.CollisionFidelity.Box, Enum.RenderFidelity.Precise)
+					
+					-- Cache the desired fidelity settings.
+					local collisionFidelity = meshPart.CollisionFidelity
+					local renderFidelity = meshPart.RenderFidelity
 
-				local dispatch: thread = MESH_DISPATCH[meshPart]
+					-- Apply the mesh.
+					meshPart:ApplyMesh(import)
 
-				if dispatch then
-					task.cancel(dispatch)
-				end
-
-				dispatch = task.defer(function()
-					local applied = false
-					local lastErr: string
-					local retries = 0
-
-					while not applied do
-						local success: boolean, response: MeshPart | string = pcall(function()
-							return InsertService:CreateMeshPartAsync(meshId, meshPart.CollisionFidelity, meshPart.RenderFidelity)
-						end)
-
-						if success and typeof(response) == "Instance" then
-							-- If the properties of what we're applying differ from what the MeshPart
-							-- currently wants, try again without increasing the retry counter.
-
-							if response.CollisionFidelity ~= meshPart.CollisionFidelity then
-								continue
-							end
-
-							if response.RenderFidelity ~= meshPart.RenderFidelity then
-								continue
-							end
-
-							meshPart:ApplyMesh(response)
-							applied = true
-
-							break
-						elseif typeof(response) == "string" then
-							lastErr = response
-
-							if lastErr:find("invalid mesh asset") then
-								break
-							end
-
-							retries += 1
-
-							if retries < 5 then
-								task.wait(retries / 2)
-							else
-								break
-							end
-						else
-							error("Unexpected response from CreateMeshPartAsync!", 2)
-						end
-					end
-
-					if not applied then
-						warn("Error creating MeshPart:", lastErr)
-					end
-
-					if MESH_DISPATCH[meshPart] == dispatch then
-						MESH_DISPATCH[meshPart] = nil
-					end
+					-- Let studio handle the fidelities in serial.
+					meshPart.CollisionFidelity = collisionFidelity
+					meshPart.RenderFidelity = renderFidelity
 				end)
 
-				MESH_DISPATCH[meshPart] = dispatch
 				return true
 			end,
 		},
